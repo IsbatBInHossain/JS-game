01d6129d44ae432fc022aeafe4c74d1e7f8daa4d
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-09-12 17:08:48 +0600

    Add sitting and running states

--- state management/player.js
+++ state management/player.js
@@ -1,4 +1,11 @@
-import { StandingRight, StandingLeft } from './state.js';
+import {
+  StandingRight,
+  StandingLeft,
+  SittingRight,
+  SittingLeft,
+  RunningRight,
+  RunningLeft,
+} from './state.js';
 
 export default class Player {
   constructor(gameWidth, gameHeight) {
@@ -6,13 +13,22 @@ export default class Player {
     this.gameHeight = gameHeight;
     this.width = 200;
     this.height = 181.833;
-    this.states = [new StandingLeft(this), new StandingRight(this)];
+    this.states = [
+      new StandingRight(this),
+      new StandingLeft(this),
+      new SittingRight(this),
+      new SittingLeft(this),
+      new RunningRight(this),
+      new RunningLeft(this),
+    ];
     this.currentState = this.states[0];
     this.image = document.getElementById('player');
     this.x = (this.gameWidth - this.width) / 2;
     this.y = this.gameHeight - this.height;
     this.frameX = 0;
     this.frameY = 0;
+    this.speed = 0;
+    this.maxSpeed = 10;
   }
   draw(context) {
     context.drawImage(
@@ -29,6 +45,11 @@ export default class Player {
   }
   update(input) {
     this.currentState.handleInput(input);
+    // horizontal movement
+    this.x += this.speed;
+    if (this.x <= 0) this.x = 0;
+    if (this.x >= this.gameWidth - this.width)
+      this.x = this.gameWidth - this.width;
   }
   setState(state) {
     this.currentState = this.states[state];
--- state management/state.js
+++ state management/state.js
@@ -1,6 +1,10 @@
 export const states = {
-  STANDING_LEFT: 0,
-  STANDING_RIGHT: 1,
+  STANDING_RIGHT: 0,
+  STANDING_LEFT: 1,
+  SITTING_RIGHT: 2,
+  SITTING_LEFT: 3,
+  RUNNING_RIGHT: 4,
+  RUNNING_LEFT: 5,
 };
 
 class State {
@@ -16,11 +20,12 @@ export class StandingLeft extends State {
   }
   enter() {
     this.player.frameY = 1;
+    this.player.speed = 0;
   }
   handleInput(input) {
-    if (input === 'PRESS right') {
-      this.player.setState(states.STANDING_RIGHT);
-    }
+    if (input === 'PRESS right') this.player.setState(states.STANDING_RIGHT);
+    else if (input === 'PRESS down') this.player.setState(states.SITTING_LEFT);
+    else if (input === 'PRESS left') this.player.setState(states.RUNNING_LEFT);
   }
 }
 export class StandingRight extends State {
@@ -30,10 +35,77 @@ export class StandingRight extends State {
   }
   enter() {
     this.player.frameY = 0;
+    this.player.speed = 0;
   }
   handleInput(input) {
-    if (input === 'PRESS left') {
+    if (input === 'PRESS left') this.player.setState(states.STANDING_LEFT);
+    else if (input === 'PRESS down') this.player.setState(states.SITTING_RIGHT);
+    else if (input === 'PRESS right')
+      this.player.setState(states.RUNNING_RIGHT);
+  }
+}
+export class SittingRight extends State {
+  constructor(player) {
+    super('SITTING_RIGHT');
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 8;
+    this.player.speed = 0;
+  }
+  handleInput(input) {
+    if (input === 'PRESS up') this.player.setState(states.STANDING_RIGHT);
+    else if (input === 'PRESS left') this.player.setState(states.SITTING_LEFT);
+    else if (input === 'RELEASE down')
+      this.player.setState(states.STANDING_RIGHT);
+  }
+}
+export class SittingLeft extends State {
+  constructor(player) {
+    super('SITTING_LEFT');
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 9;
+    this.player.speed = 0;
+  }
+  handleInput(input) {
+    if (input === 'PRESS up') this.player.setState(states.STANDING_LEFT);
+    else if (input === 'PRESS right')
+      this.player.setState(states.SITTING_RIGHT);
+    else if (input === 'RELEASE down')
       this.player.setState(states.STANDING_LEFT);
-    }
+  }
+}
+export class RunningRight extends State {
+  constructor(player) {
+    super('RUNNING_RIGHT');
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 6;
+    this.player.speed = this.player.maxSpeed;
+  }
+  handleInput(input) {
+    if (input === 'PRESS left') this.player.setState(states.RUNNING_LEFT);
+    else if (input === 'RELEASE right')
+      this.player.setState(states.STANDING_RIGHT);
+    else if (input === 'PRESS down') this.player.setState(states.SITTING_RIGHT);
+  }
+}
+export class RunningLeft extends State {
+  constructor(player) {
+    super('RUNNING_LEFT');
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 7;
+    this.player.speed = -this.player.maxSpeed;
+  }
+  handleInput(input) {
+    if (input === 'PRESS right') this.player.setState(states.RUNNING_RIGHT);
+    else if (input === 'RELEASE left')
+      this.player.setState(states.STANDING_LEFT);
+    else if (input === 'PRESS down') this.player.setState(states.SITTING_LEFT);
   }
 }
